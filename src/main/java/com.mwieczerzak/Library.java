b5180c52e14374db9cb300e3557cbd38cefd6b35
mwieczerzak <mwieczerzak09@gmail.com>
2018-07-15 16:31:34 +0200

    save to file added

--- src/main/java/com.mwieczerzak/Library.java
+++ src/main/java/com.mwieczerzak/Library.java
@@ -1,10 +1,15 @@
 package com.mwieczerzak;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Library {
+
+    private static final String FILENAME = "teamLibrary.txt";
 
     private List<Team> teams;
 
@@ -32,18 +37,50 @@ public class Library {
                 .collect(Collectors.toList());
     }
 
-    public List<Player> findPlayersByPosition(Position position){
+    public List<Player> findPlayersByPosition(Position position) {
         return teams.stream()
                 .flatMap(team -> team.findPlayersByPosition(position).stream())
                 .collect(Collectors.toList());
     }
 
-    public List<Team> findTeamByFifaRankingPosition(int fifaRankingPosition){
+    public List<Team> findTeamByFifaRankingPosition(int fifaRankingPosition) {
         return teams.stream()
                 .filter(team -> team.getFifaRankingPosition() == fifaRankingPosition)
                 .collect(Collectors.toList());
-
     }
+
+    public void saveToFile() {
+        try {
+            File file = new File(FILENAME);
+            PrintWriter printWriter = new PrintWriter(file);
+            printWriter.println(teams.size());
+            for (Team team : teams) {
+                saveTeamToFile(printWriter, team);
+            }
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Problem with saving to a file " + FILENAME);
+        }
+    }
+
+    private void saveTeamToFile(PrintWriter printWriter, Team team) {
+        printWriter.println(team.getNationality());
+        printWriter.println(team.getTrainer());
+        printWriter.println(team.getPlayers().size());
+        for (Player player : team.getPlayers()) {
+            savePlayerToFile(printWriter, player);
+        }
+        printWriter.println(team.getFifaRankingPosition());
+    }
+
+    private void savePlayerToFile(PrintWriter printWriter, Player player) {
+        printWriter.println(player.getFirstName());
+        printWriter.print(player.getLastName());
+        printWriter.print(player.getAge());
+        printWriter.print(player.getPosition());
+        printWriter.print(player.getClub());
+    }
+
 
 
 }
--- src/test/java/com/mwieczerzak/TeamTest.java
+++ src/test/java/com/mwieczerzak/TeamTest.java
@@ -16,10 +16,11 @@ public class TeamTest {
         Player player3 = new PlayerBuilder()
                 .withAge(23)
                 .build();
-        Team team = new Team("nationality", "trainer", 10);
-        team.addPlayer(player1);
-        team.addPlayer(player2);
-        team.addPlayer(player3);
+        Team team = new TeamBuilder()
+                .withPlayer(player1)
+                .withPlayer(player2)
+                .withPlayer(player3)
+                .build();
         Assertions.assertTrue(team.getAverageAgeOfPlayers() == (double)(22 + 28 + 23) / 3);
     }
 
@@ -31,9 +32,10 @@ public class TeamTest {
         Player player2 = new PlayerBuilder()
                 .withPosition(Position.FORWARD)
                 .build();
-        Team team = new Team("nationality", "trainer", 10);
-        team.addPlayer(player1);
-        team.addPlayer(player2);
+        Team team = new TeamBuilder()
+                .withPlayer(player1)
+                .withPlayer(player2)
+                .build();
         Assertions.assertTrue(team.getPlayerPositions().size() == 2);
         Assertions.assertTrue(team.getPlayerPositions().contains(Position.DEFENDER));
         Assertions.assertTrue(team.getPlayerPositions().contains(Position.FORWARD));
