a1b6c0879247690616bf2d7e4730765d73d8e934
mwieczerzak <mwieczerzak09@gmail.com>
2018-07-17 14:12:42 +0200

    unit tests for Team class added

--- src/test/java/com/mwieczerzak/TeamTest.java
+++ src/test/java/com/mwieczerzak/TeamTest.java
@@ -2,11 +2,15 @@ package com.mwieczerzak;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import sun.util.resources.cldr.eo.LocaleNames_eo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeamTest {
 
     @Test
-    public void shouldGetAverageAgeOfPlayers(){
+    public void shouldGetAverageAgeOfPlayers() {
         Player player1 = new PlayerBuilder()
                 .withAge(22)
                 .build();
@@ -21,6 +25,35 @@ public class TeamTest {
                 .withPlayer(player2)
                 .withPlayer(player3)
                 .build();
-        Assertions.assertTrue(team.getAverageAgeOfPlayers() == (double)(22 + 28 + 23) / 3);
+        Assertions.assertTrue(team.getAverageAgeOfPlayers() == (double) (22+28+23) / 3);
+    }
+
+    @Test
+    public void shouldFindPlayerByLastName() {
+        Player player1 = new PlayerBuilder()
+                .withLastName("Ozil")
+                .build();
+        Player player2 = new PlayerBuilder()
+                .withLastName("Draxler")
+                .build();
+        Team team = new TeamBuilder()
+                .withPlayer(player1)
+                .withPlayer(player2)
+                .build();
+        Assertions.assertEquals(team.getPlayers().subList(0,1), team.findPlayerByLastName(player1.getLastName()));
+    }
+
+    @Test
+    public void shouldFindPlayersByPosition(){
+        Player player1 = new Player("Leo", "Messi", 25, Position.FORWARD, "Barcelona FC");
+        Player player2 = new Player("Cristiano", "Ronaldo", 30, Position.FORWARD, "Juventus FC");
+        Player player3 = new Player("Petr", "Cech", 31, Position.GOALKEEPER, "Arsenal FC");
+        Team team = new TeamBuilder()
+                .withPlayer(player1)
+                .withPlayer(player2)
+                .withPlayer(player3)
+                .build();
+        Assertions.assertTrue(team.findPlayersByPosition(Position.FORWARD).size() == 2);
+
     }
 }
